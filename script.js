44bb4e07b5e5c3524e3a68de18b6125e2adf89b1
Arham Ali <ara1399@mavs.uta.edu>
2024-03-05 21:34:44 -0600

    Udpate

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let speech = new SpeechSynthesisUtterance();
+let speech = new SpeechSynthesisUtterance(); //API
 let voices = [];
-let voiceSelect = document.querySelector("select");
+let voiceSelect = document.querySelector("select"); //Select voice
 
 window.speechSynthesis.onvoiceschanged = () => {
-  voices = window.speechSynthesis.getVoices();
+  voices = window.speechSynthesis.getVoices(); //Go through the various available voices
   speech.voice = voices[0];
   voices.forEach(
     (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
   );
 };
 
-voiceSelect.addEventListener("change", () => {
+voiceSelect.addEventListener("change", () => { //Change voice
   speech.voice = voices[voiceSelect.value];
 });
 
-document.querySelector("button").addEventListener("click", () => {
+document.querySelector("button").addEventListener("click", () => { //Select voice function
   speech.text = document.querySelector("textarea").value;
   window.speechSynthesis.speak(speech);
 });
